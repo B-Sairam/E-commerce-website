8a739e5d7a3429948d16bbd3705c095827a21add
SAIRAM <87373136+B-Sairam@users.noreply.github.com>
2021-11-18 19:33:12 +0530

    .

--- src/App.js
+++ src/App.js
@@ -22,8 +22,8 @@ function App() {
         box.style.display="none";
         
     }
-    function view_cart(e){
-        e.preventDefault();
+    function view_cart(){
+       
         let box = document.querySelector(".cart-box");
         box.style.display="inline";
 
@@ -34,7 +34,7 @@ function App() {
     <>
         <nav className=" navbar navbar-expand-lg navbar-light bg-light ">
             <div className="container px-4 px-lg-5">
-                <a class="navbar-brand bg-warning p-2" href="#!" >TYLES WEAR</a>
+                <a class="navbar-brand bg-warning p-2" href="#!" >STYLES WEAR</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
@@ -51,11 +51,11 @@ function App() {
                         </li>
                     </ul>
                     <form class="d-flex">
-                        <button class="btn btn-outline-dark" onClick={()=>view_cart() }>
+                        <a class="btn btn-outline-dark" onClick={()=>view_cart() }>
                             <i class="bi-cart-fill me-1"></i>
                             Cart
                             <span class="badge bg-dark text-white ms-1 rounded-pill">{cart}</span>
-                        </button>
+                        </a>
                     </form>
                     <div>
                         <button class=" price-tag btn btn-outline-ligth ">
